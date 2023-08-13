2ebe8dcbb8ed174a63c73526054138b5eb00701b
markoton <markotoncanovic@gmail.com>
2023-08-13 13:09:20 +0200

    add comment

--- streams.js
+++ streams.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 console.log(fs);
 
+// encoding: utf8 - i onda 'chunk' odmah pise u text umesto u <bufer...>
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8'});
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
 readStream.on('data', chunk => {
-  // console.log('---- NEW CHUNK ----');
-  // console.log(chunk);
+  console.log('---- NEW CHUNK ----');
+  console.log(chunk);
   writeStream.write('\nNEW CHUNK:\n');
   writeStream.write(chunk);
 });
